b0c3e8eed31dd9d060f154207bf92b074754a47d
LUA-CHAVES-SPT <lua.chaves@sptech.school>
2025-12-11 10:45:39 -0300

    Banco de Dados Criado

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -6,55 +6,74 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
+CREATE DATABASE caketech;
 
-USE aquatech;
+USE caketech;
 
 CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+razao_social VARCHAR(50),
+cnpj CHAR(14),
+codigo_ativacao VARCHAR(50)
 );
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
+idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50),
+email VARCHAR(50),
+senha VARCHAR(50),
+fk_empresa INT,
+FOREIGN KEY (fk_empresa) REFERENCES empresa(idEmpresa)
+);
+
+create table armazem (
+/* em nossa regra de negócio, um armazem tem apenas um sensor */
+	idArmazem INT PRIMARY KEY AUTO_INCREMENT,
 	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+    cep CHAR(8),
+	numCep INT,
+    complemento CHAR(50),
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(idEmpresa)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+
+CREATE TABLE produto (
+	idProduto INT PRIMARY KEY AUTO_INCREMENT,
+	preco DECIMAL(5,2),
+	sabor VARCHAR(150),
+	qtdProdutos INT,
+    fk_armazem INT,
+	FOREIGN KEY (fk_armazem) REFERENCES armazem(idArmazem)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+-- ALTER TABLE produto MODIFY COLUMN preco DECIMAL(5,2);
 
 create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+	idMedida INT PRIMARY KEY AUTO_INCREMENT,
+	fk_produto INT,
+    dht11_umidade DECIMAL(3,2),
+	dht11_temperatura DECIMAL(3,2),
+	registro DATETIME,
+	FOREIGN KEY (fk_produto) REFERENCES produto(idProduto)
 );
+
+create table pedido (
+idPedido INT,
+pk_usuario INT,
+pk_pedido INT,
+titulo VARCHAR(70),
+hrPedido DATETIME default current_timestamp,
+hrEntrega DATETIME,
+statuss TINYINT(2),
+nomeCliente VARCHAR(45),
+	CONSTRAINT chkStatus CHECK (statuss IN (0,1)),
+    PRIMARY KEY (idPedido,pk_usuario, pk_pedido),
+    FOREIGN KEY (pk_usuario) REFERENCES usuario(idUsuario),
+    FOREIGN KEY (pk_pedido) REFERENCES pedido(idPedido)
+);
+
+
+
 
 insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
 insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
